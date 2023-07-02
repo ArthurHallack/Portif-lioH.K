759ba27547e7d22a8b1bf3de1cff572068377642
Arthur <ArthurHallack2@gmail.com>
2023-07-01 23:40:55 -0300

    portifólio

---
+++ javascript/script.js
@@ -0,0 +1,3 @@
+const StartBTN = window.document.getElementById ('Titulo2Top')
+const ImgCavaleiro = window.document.getElementById ('cavaleiro')
+const InformSobre = window.document.getElementById ('DivAreaSobre')
