9afaf2a224eba542eae83db977118073c3cc90e2
Arthur <ArthurHallack2@gmail.com>
2023-07-02 20:18:46 -0300

    portifólio

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,14 @@
 const StartBTN = window.document.getElementById ('Titulo2Top')
 const ImgCavaleiro = window.document.getElementById ('cavaleiro')
 const InformSobre = window.document.getElementById ('DivAreaSobre')
+const ImgProjetoPHP = window.document.getElementById ('ImagemAmigoSecreto')  
+const ImgProjetoCrono = window.document.getElementById ('ProjetoCronometro')
+
+
+/*Funções*/
+
+function scrollToSection () {
+    const section = window.document.getElementById ('SobreMimTitulo');
+    section.scrollIntoView ({behavior: "smooth"});
+
+}
