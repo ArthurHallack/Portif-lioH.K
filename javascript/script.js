9bbba1a2a6eb2a8832bc2af8e6d3270d57696f6b
Arthur <ArthurHallack2@gmail.com>
2023-07-09 22:19:58 -0300

    portifólio

--- javascript/script.js
+++ javascript/script.js
@@ -4,11 +4,50 @@ const InformSobre = window.document.getElementById ('DivAreaSobre')
 const ImgProjetoPHP = window.document.getElementById ('ImagemAmigoSecreto')  
 const ImgProjetoCrono = window.document.getElementById ('ProjetoCronometro')
 
-
 /*Funções*/
 
 function scrollToSection () {
     const section = window.document.getElementById ('SobreMimTitulo');
     section.scrollIntoView ({behavior: "smooth"});
+
+}
+
+function OpenModalHTML () {
+    document.getElementById ('ModalHTML').style.display = "block";
+}
+
+function OpenModalCSS () {
+    document.getElementById ('ModalCSS').style.display = "block";
+}
+
+function OpenModalJS () {
+    document.getElementById ('ModalJS').style.display = "block";
+}
+
+function OpenModalNode () {
+    document.getElementById ('ModalNode').style.display = "block";
+}
+
+function OpenModalGit () {
+    document.getElementById ('ModalGit').style.display = "block";
+}
+
+function CloseModalHTML () {
+
+}
+
+function CloseModalCSS () {
+
+}
+
+function CloseModalJS () {
+
+}
+
+function CloseModalNode () {
+
+}
+
+function CloseModalGit () {
 
 }
